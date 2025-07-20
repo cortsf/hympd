d05cd1dff23cbbd655c7861e871a3bc61a002980
Franco Cortesi <cortesifranco3@gmail.com>
2025-07-20 09:33:01 -0300

    Include Utility module

---
+++ app/Utility.hs
@@ -0,0 +1,17 @@
+module Utility where
+import qualified Network.MPD as MPD
+
+unId :: MPD.Id -> Int
+unId (MPD.Id n) = n
+
+infix 9 !!?
+(!!?) :: [a] -> Int -> Maybe a
+(!!?) xs i
+    | i < 0     = Nothing
+    | otherwise = go i xs
+  where
+    go :: Int -> [a] -> Maybe a
+    go 0 (x:_)  = Just x
+    go j (_:ys) = go (j - 1) ys
+    go _ []     = Nothing
+{-# INLINE (!!?) #-}
