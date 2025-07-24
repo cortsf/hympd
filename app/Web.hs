5baf5bde5f40636e4f516c38f6e9e2c0d8ba61a6
Franco Cortesi <cortesifranco3@gmail.com>
2025-07-24 20:03:23 -0300

    Set containter margin on text instead of padding div.

--- app/Web.hs
+++ app/Web.hs
@@ -45,7 +45,7 @@ page current_page content = do
       style_ $ T.pack "#playerProgressInput{-webkit-appearance: none; background: oklch(55.6% 0 0); background-image: linear-gradient(#FFD6A8, #FFD6A8); background-size: " <> current_time <> "% 100%; background-repeat: no-repeat;}#playerProgressInput::-webkit-slider-thumb {-webkit-appearance: none; height: 0px; width: 0px;}"
     body_ [class_ "overflow-y-scroll flex flex-col bg-blue-200 dark:bg-gray-900 focus:outline-none dark:text-slate-400"] $ do
       nav_full current_page
-      div_ [id_ "content", class_ "overflow-y-visible max-w-screen-xl w-full grow flex flex-col mx-auto pt-4 pb-8 bg-white dark:bg-slate-800 [&_tr]:odd:bg-slate-50 [&_tr]:odd:dark:bg-slate-700 [&_tr]:even:bg-white [&_tr]:even:dark:bg-slate-800 [&_tr]:dark:hover:bg-sky-900"] $ do
+      div_ [id_ "content", class_ "overflow-y-visible max-w-screen-xl w-full grow flex flex-col mx-auto pt-4 bg-white dark:bg-slate-800 [&_tr]:odd:bg-slate-50 [&_tr]:odd:dark:bg-slate-700 [&_tr]:even:bg-white [&_tr]:even:dark:bg-slate-800 [&_tr]:dark:hover:bg-sky-900"] $ do
         content
       script_ $ "feather.replace();"
       script_ $ jsblock
@@ -113,7 +113,7 @@ queuePage = do
         button_ [onclick_ "socket.send('clear')", class_ "px-2 py-1 rounded-md text-white cursor-pointer flex items-center"] (i_ [class_ "size-5 stroke-2", data_ "feather" "file"] "" <> span_ [class_ "ml-1"] "Clear")
     case playlist of
       Left _ -> p_ "playlist error"
-      Right [] -> div_ [class_ "px-10 mt-4 text-rose-400"] $ p_ "-Empty queue-"
+      Right [] -> div_ [class_ "px-10 mt-4 mb-8 text-rose-400"] $ p_ "-Empty queue-"
       Right pl -> table_ [class_ "table-auto w-full mt-4"] $ do
         tbody_ $ mapM_ (\song ->
                           tr_ [class_ "flex place-content-between px-2 my-0"] $ do
