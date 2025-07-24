6537ac6fb2924f6dfec476ad58edec7c03f573b0
Franco Cortesi <cortesifranco3@gmail.com>
2025-07-24 02:42:16 -0300

    Simplify progress bar style

--- app/Web.hs
+++ app/Web.hs
@@ -37,8 +37,7 @@ page current_page content = do
       script_ [src_ "static/styles.css"] ("" :: String)
       script_ [src_ "static/icons.js"] ("" :: String)
       link_ [rel_ "icon", href_ "static/favicon4.png", sizes_ "any", type_ "image/png"]
-      style_ $ T.pack "#playerProgressInput{-webkit-appearance: none; background: rgba(255, 255, 255, 0.6); border-radius: 5px; background-image: linear-gradient(#FFA100, #FFA100); background-size: 70% 100%; background-repeat: no-repeat;}"
-      style_ $ T.pack "#playerProgressInput::-webkit-slider-thumb {-webkit-appearance: none; height: 0px; width: 0px; cursor: ew-resize; box-shadow: 0 0 2px 0 #555; transition: background .3s ease-in-out;}"
+      style_ $ T.pack "#playerProgressInput{-webkit-appearance: none; background: rgba(255, 255, 255, 0.6); background-image: linear-gradient(#FFA100, #FFA100); background-size: 0% 100%; background-repeat: no-repeat;}#playerProgressInput::-webkit-slider-thumb {-webkit-appearance: none; height: 0px; width: 0px;}"
     body_ [class_ "overflow-y-scroll flex flex-col bg-blue-200 dark:bg-gray-900 focus:outline-none dark:text-slate-400"] $ do
       nav_full current_page
       div_ [id_ "content", class_ "overflow-y-visible max-w-screen-xl w-full grow flex flex-col mx-auto pt-8 bg-white dark:bg-slate-800 [&_tr]:odd:bg-slate-50 [&_tr]:odd:dark:bg-slate-700 [&_tr]:even:bg-white [&_tr]:even:dark:bg-slate-800 [&_tr]:dark:hover:bg-sky-900"] $ do
