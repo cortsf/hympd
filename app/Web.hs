5823c15ffae08c48b8d422319bb65bb2f0126772
Franco Cortesi <cortesifranco3@gmail.com>
2025-07-26 09:49:25 -0300

    Encode URIs (Queue page)

--- app/Web.hs
+++ app/Web.hs
@@ -140,7 +140,7 @@ queuePage options = do
                                div_ [class_ "text-ellipsis grow flex place-content-start"] $ toHtml $ guessTitle song
                                div_ [class_ "px-2"] $ toHtml $ formatTime defaultTimeLocale (if MPD.sgLength song > 3600 then "%H:%M:%S" else "%M:%S") $ posixSecondsToUTCTime $ fromIntegral $ MPD.sgLength song
                            td_ [class_ "flex gap-x-2"] $ do
-                             a_ [href_ $ "/browse?path=" <> (T.pack $ FP.takeDirectory $ MPD.toString $  MPD.sgFilePath song), class_ "pl-0 pr-4 w-1 hover:dark:text-blue-600 dark:text-blue-300 cursor-pointer my-auto"] $ i_ [class_ "size-4 stroke-3", data_ "feather" "search"] ""
+                             a_ [href_ $ "/browse?path=" <> (U.encodeText $ T.pack $ FP.takeDirectory $ MPD.toString $  MPD.sgFilePath song), class_ "pl-0 pr-4 w-1 hover:dark:text-blue-600 dark:text-blue-300 cursor-pointer my-auto"] $ i_ [class_ "size-4 stroke-3", data_ "feather" "search"] ""
                              button_ [onclick_ (maybe "alert('Error: No id')" (\x -> "socket.send('deleteId," <> (T.pack $ show $ unId x) <> "')")  (MPD.sgId song)), class_ "pl-0 pr-4 w-1 text-red-300 hover:text-red-400 dark:text-rose-300 cursor-pointer"] $ i_ [class_ "size-4 stroke-2", data_ "feather" "trash-2"] ""
                        ) pl
 
